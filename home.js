ffd3d90e8812dafc383994b79deb12d2554bbf0f
Duy Anh Do <doduyanh149@gmail.com>
2023-01-14 20:27:46 +0700

    check point

---
+++ home.js
@@ -0,0 +1,8 @@
+let addProduct = () => {
+    let kenght= document.getElementById("hello").value
+    let setLenght = kenght.lenght
+    if (setLenght<5) {
+        alert("chua du")
+    }
+    if 
+} 
